ee5b6161ee7bb4f43682ef19a376afd5b19079b1
DrigoArts <156488766+DrigoArts@users.noreply.github.com>
2024-01-13 21:14:36 -0300

    Add files via upload

---
+++ css/js/offline_card.js
@@ -0,0 +1,37 @@
+function abrir1x1() {
+    var url = "https://amer3-my.sharepoint.com/:x:/r/personal/rduarte_americanas_io/Documents/PAPELETAS/papeleta%201x1.xlsx?d=w00934ff7cef543f08335e45fa3c644dc&csf=1&web=1&e=Cf9vix";
+    var target = "_blank";
+    window.open(url, target);
+}
+function abrir2x1() {
+    var url = "https://amer3-my.sharepoint.com/:x:/r/personal/rduarte_americanas_io/Documents/PAPELETAS/papeleta%202x1.xlsx?d=w6869984b5e9a46db9242622bc65e0afa&csf=1&web=1&e=zmfdS8&nav=MTVfe0MwMDMyQzJDLURBQjQtNEZCMy04ODI4LTlBODg3NTU2MzBCQX0";
+    var target = "_blank";
+    window.open(url, target);
+}
+function abrirTelefonia() {
+    var url = "https://amer3-my.sharepoint.com/:x:/g/personal/rduarte_americanas_io/EbPhJiyqPxRFvALlEyQujBwBroKrcIwwC6aXMZCMK7347g?e=cmPHe7";
+    var target = "_blank";
+    window.open(url, target);
+}
+function abrirfgts1() {
+    var url = "https://amer3-my.sharepoint.com/:x:/r/personal/rduarte_americanas_io/Documents/PAPELETAS/1X1%20fgts.xlsx?d=w32fdf4ae1f7d41babdbaa1ef231ccae2&csf=1&web=1&e=zhiA7H";
+    var target = "_blank";
+    window.open(url, target);
+}
+function abrirfgts2() {
+    var url = "https://amer3-my.sharepoint.com/:x:/r/personal/rduarte_americanas_io/Documents/PAPELETAS/2X1%20fgts.xlsx?d=wa53231b929924f54a3ff65d56acce5cf&csf=1&web=1&e=RQcF1A";
+    var target = "_blank";
+    window.open(url, target);
+}
+function abrirPanfleto() {
+    var url = "https://amer3-my.sharepoint.com/:x:/r/personal/rduarte_americanas_io/Documents/PAPELETAS/FGTS.xlsx?d=w32c82efd2e564e1d8732da3c6837b046&csf=1&web=1&e=TjuVUR";
+    var target = "_blank";
+    window.open(url, target);
+}
+
+
+//function abrir4() {
+  //  var url = "https://amer3-my.sharepoint.com/:x:/r/personal/rduarte_americanas_io/Documents/PAPELETAS/1X1%20fgts.xlsx?d=w32fdf4ae1f7d41babdbaa1ef231ccae2&csf=1&web=1&e=OObjr3";
+   // var target = "_blank";
+    //window.open(url, target);
+//}
